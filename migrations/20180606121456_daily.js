1e84b8fd73c46d2640055c84fa00bafa250fdcac
Clark Newell <williamcnewell@gmail.com>
2018-06-06 17:37:30 -0600

    adding image slot

--- migrations/20180606121456_daily.js
+++ migrations/20180606121456_daily.js
@@ -1,7 +1,6 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('dailygoal', table => {
         table.increments('id')
-        table.string('date')
         table.string('goal')
         table.string('due')
         table.string('done')
--- seeds/01_daily.js
+++ seeds/01_daily.js
@@ -2,22 +2,20 @@ exports.seed = function (knex, Promise) {
   return knex('dailygoal').del().then(() => {
       return knex('dailygoal').insert([{
           id: 1,
-          date: '06/04/18',
           goal: 'Finish tower back-end',
           due: '11/04/18',
           done: 'No',
           comment: 'Getting there!',
-          image: ''
+          image: 'https://aws-tiqets-cdn.imgix.net/images/content/da0b659013eb49cf816407c5ada7bd3c.jpg?auto=format&fit=crop&ixlib=python-1.1.2&q=25&s=b765f82b2ff445149c6c30cbb8caaf35&w=330&h=330&dpr=1'
 
         },
         {
           id: 2,
-          date: '06/06/18',
           goal: 'Finish tower front-end',
           due: '11/04/18',
           done: 'No',
           comment: 'Got started!',
-          image: ''
+          image: 'https://media.istockphoto.com/photos/shanghai-landmarks-picture-id656311250?k=6&m=656311250&s=612x612&w=0&h=CGkuSdaYCd3ZKPAC0A22_-_v-_Ral8GaUFjTBEF_LEg='
         }
       ])
   })
